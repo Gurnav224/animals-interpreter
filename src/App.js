493952771f037da90bf48c29ad756ec2779613d2
Gurnav chaudhary <chaudharyg856@gmail.com>
2023-02-12 11:47:55 +0530

    animal interpreter add

--- src/App.js
+++ src/App.js
@@ -1,8 +1,53 @@
+import React, { useState } from 'react'
+import './App.css'
+
+var headingText = "inside Outt"
+var animalsDictionary = {
+  "🐒":"monkey",
+  "🐕 ":"dog",
+  "🦁" : "lion",
+  "🐅":"tiger",
+  "🦓":"zebra"
+}
+
+var animalWeknow = Object.keys(animalsDictionary);
+
+
 
 function App() {
+ const [meaning,setMeaning]  = useState("")
+  function inputChangeHandler(event){
+    var userInput = event.target.value;
+    var  meaning = animalsDictionary[userInput]
+
+    if(meaning === undefined){
+      meaning = "we don't have the meaning in Database"
+    }
+      
+    setMeaning(meaning)
+    }
+
+    function animalClickHandler(animal){
+      var meaning = animalsDictionary[animal]
+      setMeaning(meaning)
+    }
+
   return (
     <div className="App">
-     <h1> check woriking</h1>
+       <h1>{headingText}</h1>
+       <input onChange={inputChangeHandler} />
+       <h2>{meaning}</h2>
+       <h3>Animal Emoji we know</h3>
+       <div>
+        {
+          animalWeknow.map(animal => {
+            return <span onClick={()=>animalClickHandler(animal)}
+             style={{fontSize:"2.2rem",padding:"0.5rem",cursor:"pointer"}}>
+              {animal}
+            </span>
+          })
+        }
+       </div>
     </div>
   );
 }
